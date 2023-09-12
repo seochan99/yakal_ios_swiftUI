456291b93f537f0e7000a64f09dc05e88c38fb5e
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-09-12 12:23:26 +0900

    feat : inital Setting

---
+++ yakal/yakal/yakalApp.swift
@@ -0,0 +1,17 @@
+//
+//  yakalApp.swift
+//  yakal
+//
+//  Created by 서희찬 on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct yakalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
